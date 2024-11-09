6849ed58a1d6f8c9bf4d0ca282f49fcc307e30d5
roadmaus <gufe@protonmail.com>
2024-11-09 15:51:30 +0100

    check

--- main.cjs
+++ main.cjs
@@ -10,14 +10,16 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    show: false,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
       preload: path.join(__dirname, 'preload.js'),
     },
   });
+
+  mainWindow.maximize();
+  mainWindow.show();
 
   if (process.env.NODE_ENV === 'development') {
     setTimeout(() => {
